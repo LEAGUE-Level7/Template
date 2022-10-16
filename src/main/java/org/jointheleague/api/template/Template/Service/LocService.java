6c5b55c9113ca9e0f501585d26112fb5c47c6ae5
Bexdog <beckhamkunkler@gmail.com>
2022-10-16 15:48:20 -0700

    Milestone 7 Working on yoda speak error with receiving data

--- src/main/java/org/jointheleague/api/template/Template/Service/LocService.java
+++ src/main/java/org/jointheleague/api/template/Template/Service/LocService.java
@@ -1,44 +1,65 @@
 package org.jointheleague.api.template.Template.Service;
 
+import io.swagger.v3.core.util.Json;
 import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Locale;
+
 @Service
 public class LocService {
-    final String verbAPI = "https://raw.githubusercontent.com/datmt/English-Verbs/master/verbsList";
+    final String verbAPI = "https://raw.githubusercontent.com/Drulac/English-Verbs-Conjugates/master/verbs-conjugations.json";
     private final WebClient webClient = WebClient.builder().baseUrl(verbAPI).build();
-    public String getYodaSpeak(String query){
-        String end = "";
-        if(query.contains(".")) {
-            String[] sentence = query.split("\\.");
-            for(int i = 0; i<sentence.length;i++){
-                sentence[i]=sentence[i].trim();
-                String[] words = sentence[i].split(" ");
-                VERBS: for(int j = 0;j<words.length;j++){
+    String end = "";
+    Json answer = null;
+    String verb = "";
+    String[] words = new String[0];
 
-                    String answer = webClient.get()
+    public String getYodaSpeak(String query) {
+        end = "";
+        answer = null;
+        verb = "";
+        words = new String[0];
+        if (query.contains(".")) {
+            String[] sentence = query.split("\\.");
+            for (int i = 0; i < sentence.length; i++) {
+                sentence[i] = sentence[i].trim();
+                words = sentence[i].split(" ");
+                VERBS:
+                for (int j = 0; j < words.length; j++) {
+                    answer = webClient.get()
                             .uri(uriBuilder -> uriBuilder
                                     .build()
                             )
                             .retrieve()
-                            .bodyToMono(String.class)
+                            .bodyToMono(Json.class)
                             .block();
-                    if(answer.contains(words[j])){
+                    if ((answer.toString().contains('"' + words[j] + '"'))) {
+                        verb = words[j];
+                        System.out.println(verb);
                         break VERBS;
                     }
                 }
+                sentence[i] = sentence[i].substring(
+                        sentence[i].indexOf(verb) +
+                                verb.length())
+                        + ", " +
+                        sentence[i].substring(0, sentence[i].indexOf(verb) + verb.length()).toLowerCase();
+                sentence[i] = (sentence[i].charAt(0) + "").toUpperCase() + sentence[i].substring(1);
                 //I like to eat pizza
-                //pizza, I like to eat
+                //To eat pizza, I like
 
-                end+=sentence[i]+". ";
+                end += sentence[i] + ". ";
+                System.out.println(end);
             }
+        } else {
 
-            return end.substring(0,end.length()-2);
+            }
+        return end;
         }
-        else{
-            return end;
-        }
+
     }
 
-}
+
+
