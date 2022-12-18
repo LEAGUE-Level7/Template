35bd43eac7268e73acde8750bd26b2bc08f4952c
Bexdog <37968401+Bexdog@users.noreply.github.com>
2022-12-18 10:47:02 -0800

    pushing to server

--- src/main/java/org/jointheleague/api/template/Template/presentation/LocController.java
+++ src/main/java/org/jointheleague/api/template/Template/presentation/LocController.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +21,7 @@ public class LocController {
     }
 
     @GetMapping("/yoda")
+    @CrossOrigin()
     @Operation(summary = "Searches for articles matching the search term")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Result(s) found"),
